016f96acb47b97315d12ddedf7ec95f49983c4ec
Виктор Зашев <viktor@webvella.com>
2024-05-21 09:39:11 +0300

    Added the new form windows

---
+++ WindowsFormsPresentationLayer/LoginWindow.cs
@@ -0,0 +1,15 @@
+namespace WindowsFormsPresentationLayer
+{
+	public partial class LoginWindow : Form
+	{
+		public LoginWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void LoginWindow_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
