a219a1a1b4010c15f7ee951faa26ce3767f7025f
Виктор Зашев <viktor@webvella.com>
2025-03-22 08:02:59 +0200

    unfinished tests

--- PetExchangeTests/DataLayer/PetDbContextTests.cs
+++ PetExchangeTests/DataLayer/PetDbContextTests.cs
@@ -1,177 +1,94 @@
 ﻿using DataLayer;
+using NuGet.Protocol.Core.Types;
 
 namespace PetExchangeTests
 {
     public class PetDbContextTests : DataLayerTestsManagement
 	{
-		[Test]
-		public async Task CreateMethod_AddsPetModelToDatabase()
-		{
-			// Arrange
-			var newUser = new User();
-			var newPet = new Pet(newUser, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
+        [Test]
+        public async Task CreateMethod_AddsPetToDatabase()
+        {
+            var newUser = GetExampleUser();
+            var newPet = new Pet(newUser, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
 
-			var initialCount = db.Pets.Count();
+            var initialCount = db.Pets.Count();
+            await petContext.CreateAsync(newPet);
+            var actualCount = db.Pets.Count();
 
-			// Act
-			await petContext.CreateAsync(newPet);
-			var actualCount = db.Pets.Count();
-			var expectedCount = initialCount + 1;
+            Assert.That(actualCount, Is.EqualTo(initialCount + 1), "Pet count did not increment by 1.");
+        }
 
-			// Assert
-			Assert.That(expectedCount, Is.EqualTo(actualCount), "The count of existing pets in database doesn't increment by 1 when adding one pet model!");
-		}
+        [Test]
+        public async Task CreateMethod_ThrowsExceptionForDuplicateKey()
+        {
+            var user = GetExampleUser();
+            var matchingId = Guid.NewGuid();
+            var newPet = new Pet(user, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = matchingId };
+            var duplicatePet = new Pet(user, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = matchingId };
 
-		[Test]
-		public async Task CreateMethod_ThrowsExceptionWhenTryingToAddDuplicateKey()
-		{
-			// Arrange
-			var user = new User();
-			var matchingId = Guid.NewGuid();
-			var newPet = new Pet(user, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
-			var duplicatePet = new Pet(user, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
-			newPet.Id = matchingId;
-			duplicatePet.Id = matchingId;
+            await petContext.CreateAsync(newPet);
+            Assert.ThrowsAsync<InvalidOperationException>(async () => await petContext.CreateAsync(duplicatePet));
+        }
 
-			// Act & Assert
-			await petContext.CreateAsync(newPet);
-			Assert.Throws<InvalidOperationException>(() =>  petContext.CreateAsync(duplicatePet));
-		}
+        [Test]
+        public async Task ReadMethod_RetrievesPetFromDatabase()
+        {
+            var id = Guid.NewGuid();
+            var enteredPet = new Pet(GetExampleUser(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = id };
+            db.Pets.Add(enteredPet);
+            await db.SaveChangesAsync();
 
-		[Test]
-		public async Task ReadMethod_RetrievesAPetModelFromDatabase()
-		{
-			// Arrange
-			var id = Guid.NewGuid();
-			var enteredPet = new Pet(new User(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = id };
-			enteredPet.Id = id;
-			// Act
-			db.Pets.Add(enteredPet);
-			db.SaveChanges();
-			var actualPet = await petContext.ReadAsync(id);
+            var actualPet = await petContext.ReadAsync(id);
+            Assert.That(actualPet,Is.EqualTo(enteredPet), "Read method did not return the correct pet.");
+        }
 
-			// Assert
-			Assert.That(actualPet, Is.EqualTo(enteredPet), "Read method doesn't return the pet model entered in database!");
-		}
+        [Test]
+        public async Task ReadAllMethod_ReturnsAllPets()
+        {
+            db.Pets.AddRange(
+                new Pet(GetExampleUser(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false),
+                new Pet(GetExampleUser(), "Buddy", "", 3, PetTypeEnum.Dog, "A playful dog", false)
+            );
+            await db.SaveChangesAsync();
 
-		[Test]
-		public async Task UpdateMethod_UpdatesPetModelInDatabase()
-		{
-			// Arrange
-			var id = Guid.NewGuid();
-			var initialPet = new Pet(new User(), "Initial Name", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = id };
-			var updatedPet = new Pet(new User(), "Updated Name", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = id };
+            var pets = await petContext.ReadAllAsync();
+            Assert.That(pets.Count, Is.EqualTo(2), "ReadAll method did not return correct number of pets.");
+        }
 
-			db.Pets.Add(initialPet);
-			db.SaveChanges();
+        [Test]
+        public async Task UpdateMethod_UpdatesPetDetails()
+        {
+            var pet = new Pet(GetExampleUser(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
+            db.Pets.Add(pet);
+            await db.SaveChangesAsync();
 
-			// Act
-			await petContext.UpdateAsync(updatedPet);
-			var actualPet = db.Pets.FirstOrDefault(p => p.Id == id);
+            pet.Name = "UpdatedFluffy";
+            await petContext.UpdateAsync(pet);
 
-			// Assert
-			Assert.That(actualPet.Name, Is.EqualTo(updatedPet.Name), "Update method doesn't update the pet model in the database!");
-		}
+            var updatedPet = await petContext.ReadAsync(pet.Id);
+            Assert.That(updatedPet.Name, Is.EqualTo("UpdatedFluffy"), "Pet name was not updated correctly.");
+        }
 
-		[Test]
-		public async Task UpdateMethod_ThrowsExceptionWhenPetDoesNotExist()
-		{
-			// Arrange
-			var nonExistentId = Guid.NewGuid();
-			var petToUpdate = new Pet(new User(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false) ;
+        [Test]
+        public async Task DeleteMethod_SetsPetAsInactive()
+        {
+            var pet = new Pet(GetExampleUser(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
+            db.Pets.Add(pet);
+            await db.SaveChangesAsync();
 
-			// Act & Assert
-			Assert.Throws<ArgumentException>(() => petContext.UpdateAsync(petToUpdate), "Update method doesn't throw an exception when the pet does not exist in the database!");
-		}
+            await petContext.DeleteAsync(pet.Id);
+            var deletedPet = await petContext.ReadAsync(pet.Id);
+            Assert.That(deletedPet.IsActive, Is.False, "Pet was not set to inactive.");
+        }
 
-		[Test]
-		public async Task DeleteMethod_RemovesPetModelFromDatabase()
-		{
-			// Arrange
-			var id = Guid.NewGuid();
-			var petToDelete = new Pet(new User(), "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false) { Id = id };
-
-			db.Pets.Add(petToDelete);
-			db.SaveChanges();
-
-			// Act
-			await petContext.DeleteAsync(id);
-			var actualPet = db.Pets.FirstOrDefault(p => p.Id == id);
-
-			// Assert
-			Assert.IsNull(actualPet, "Delete method doesn't remove the pet model from the database!");
-		}
-
-		[Test]
-		public async Task ReadAllMethod_RetrievesAllPetModelsFromDatabase()
-		{
-			// Arrange
-			var user = new User();
-			var enteredPet1 = new Pet(user, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
-			var enteredPet2 = new Pet(user, "Buddy", "", 3, PetTypeEnum.Dog, "A friendly dog", true);
-			db.Pets.Add(enteredPet1);
-			db.Pets.Add(enteredPet2);
-			db.SaveChanges();
-
-			// Act
-			var outputedPets = await petContext.ReadAllAsync();
-
-			// Assert
-			Assert.That(outputedPets.Count, Is.EqualTo(2), "ReadAll method doesn't return all entries found in the database!");
-			Assert.IsTrue(outputedPets.Any(p => p.Name == "Fluffy"), "ReadAll method doesn't return the correct pet models from the database!");
-			Assert.IsTrue(outputedPets.Any(p => p.Name == "Buddy"), "ReadAll method doesn't return the correct pet models from the database!");
-		}
-
-		[Test]
-		public async Task ReadAllMethod_ReturnsEmptyListWhenThereAreNoPetModelsInDatabase()
-		{
-			// Arrange
-			// Ensure database is empty
-
-			// Act
-			var outputedPets = await petContext.ReadAllAsync();
-
-			// Assert
-			Assert.That(outputedPets, Is.Empty, "ReadAll method doesn't return an empty list when no entries exist in the database!");
-		}
-
-		[Test]
-		public async Task ReadAllMethod_IncludesNavigationalPropertiesWhenSpecified()
-		{
-			// Arrange
-			var user = new User();
-			var enteredPet = new Pet(user, "Fluffy", "", 2, PetTypeEnum.Cat, "A cute cat", false);
-			db.Pets.Add(enteredPet);
-			db.SaveChanges();
-
-			// Act
-			var outputedPets = await petContext.ReadAllAsync(useNavigationalProperties: true);
-
-			// Assert
-			Assert.IsNotNull(outputedPets.First().User, "ReadAll method doesn't include navigational properties when specified!");
-		}
-
-		
-
-		[Test]
-		public async Task ReadMethod_ReturnsNullWhenPetDoesNotExist()
-		{
-			// Arrange
-			var nonExistentId = Guid.NewGuid();
-
-			// Act & Assert
-			Assert.That(petContext.ReadAsync(nonExistentId),Is.EqualTo(null), "Read method doesn't throw an exception when the pet does not exist in the database!");
-		}
-
-
-		[Test]
-		public async Task DeleteMethod_ThrowsExceptionWhenPetDoesNotExist()
-		{
-			// Arrange
-			var nonExistentId = Guid.NewGuid();
-
-			// Act & Assert
-			Assert.Throws<ArgumentException>(() => petContext.DeleteAsync(nonExistentId), "Delete method doesn't throw an exception when the pet does not exist in the database!");
-		}
-	}
+        public User GetExampleUser()
+        {
+            return new User()
+            {
+                Email = "example@gmail.com",
+                PhoneNumber = "0885328493",
+                UserName = "TobiasRieper"
+            };
+        }
+    }
 }
