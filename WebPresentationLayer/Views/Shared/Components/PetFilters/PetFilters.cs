3043b0166360f8db6b87fe801a561c72a0093235
Виктор Зашев <viktor@webvella.com>
2025-04-11 19:27:02 +0300

    Added responsive design improvements to pets page. Project has been successfully hosted on a server. Thesis Finished

---
+++ WebPresentationLayer/Views/Shared/Components/PetFilters/PetFilters.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace WebPresentationLayer.Components;
+
+public class PetFilters : ViewComponent
+{
+	public List<FilterGroup> Filters { get; set; }
+	public async Task<IViewComponentResult> InvokeAsync(List<FilterGroup> filters)
+	{
+		Filters = filters;
+
+        return View("PetFilters", this);
+	}
+}
